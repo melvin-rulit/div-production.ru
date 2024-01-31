ccf75ed19355913c987220a4156abc78127d2e3d
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-31 04:21:52 +0200

    создал файлы для создания Заявки

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Application\IndexController;
+use App\Http\Controllers\Api\V1\Application\CreateController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -10,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/v1/getApplications', IndexController::class);
+Route::post('/v1/createApplications', CreateController::class);
