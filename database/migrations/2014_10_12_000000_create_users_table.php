1c9dbd765878b1f7926fb684d310fc01c0b8e5de
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-31 02:15:32 +0200

    Добавление миграций

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,8 +18,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number', 32)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id')->default(User::ROLE_CLIENT);
             $table->rememberToken();
             $table->timestamps();
         });
