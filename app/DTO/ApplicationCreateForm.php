2c239bdf4817bde080677536733abca2316c74b2
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-02-04 00:57:28 +0200

    добавил DTO для создания Заявки

---
+++ app/DTO/ApplicationCreateForm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DTO;
+
+use Illuminate\Http\Request;
+
+class ApplicationCreateForm
+{
+    public string $name;
+    public string $email;
+    public string $message;
+    public string $status;
+
+    public function __construct(string $name, string $email, string $message)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->message = $message;
+    }
+
+    public static function fromRequest(Request $request): ApplicationCreateForm
+    {
+        return new static(
+            $request->get('name'),
+            $request->get('email'),
+            $request->get('message'),
+        );
+    }
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'message' => $this->message,
+        ];
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+}
--- app/Http/Controllers/Api/V1/Application/CreateController.php
+++ app/Http/Controllers/Api/V1/Application/CreateController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers\Api\V1\Application;
 
-use App\Models\Application;
-use Illuminate\Http\JsonResponse;
-use App\Http\Resources\ApplicationResource;
+use App\DTO\ApplicationCreateForm;
 use App\Http\Controllers\Api\V1\BaseController;
 use App\Http\Requests\Application\CreateRequest;
+use App\Http\Resources\ApplicationResource;
+use App\Models\Application;
+use Illuminate\Http\JsonResponse;
 
 {
     class CreateController extends BaseController
     {
+
         public function __invoke(CreateRequest $request): JsonResponse
         {
-            $validateApplication = $request->validated();
+            $data = ApplicationCreateForm::fromRequest($request)->toArray();
 
-            $result = Application::create($validateApplication);
+            $result = Application::create($data);
 
             return $this->sendResponse('Заявка была успешно создана', ApplicationResource::make($result));
         }
