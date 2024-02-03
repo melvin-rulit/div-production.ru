ebc6dc4f19d6e6847245e6ef84c65261d830844b
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-02-03 22:24:38 +0200

    создаю файлы Services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Services\ApplicationService;
 use Illuminate\Support\ServiceProvider;
+use App\Services\Interfaces\ApplicationServiceInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ApplicationServiceInterface::class,
+            ApplicationService::class);
+
     }
 
     /**
